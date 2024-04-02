ce52128be910e30722c1384a7b0fe162917e9d1a
IT INTERN <IT-INTERN@MRSHOLDINGS.LOCAL>
2024-04-02 05:00:22 -0700

    new changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,45 +58,11 @@
                                 <div class="mt-4 d-grid">
                                     <button class="btn btn-primary waves-effect waves-light" type="submit">Register</button>
                                 </div>
-
-                                <div class="mt-4 text-center">
-                                    <h5 class="font-size-14 mb-3">Sign up using</h5>
-    
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item">
-                                            <a href="javascript::void()" class="social-list-item bg-primary text-white border-primary">
-                                                <i class="mdi mdi-facebook"></i>
-                                            </a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript::void()" class="social-list-item bg-info text-white border-info">
-                                                <i class="mdi mdi-twitter"></i>
-                                            </a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript::void()" class="social-list-item bg-danger text-white border-danger">
-                                                <i class="mdi mdi-google"></i>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-        
-                                <div class="mt-4 text-center">
-                                    <p class="mb-0">By registering you agree to the Skote <a href="#" class="text-primary">Terms of Use</a></p>
-                                </div>
                             </form>
                         </div>
     
                     </div>
                 </div>
-                <div class="mt-5 text-center">
-                    
-                    <div>
-                        <p>Already have an account ? <a href="auth-login.html" class="fw-medium text-primary"> Login</a> </p>
-                        <p>© <script>document.write(new Date().getFullYear())</script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
-                    </div>
-                </div>
-
             </div>
         </div>
     </div>
